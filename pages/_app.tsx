ab1cd8846987271be53972c0f944e1694ebc2df5
dimkk <dima.v@dimkk.be>
2021-07-30 15:01:09 +0300

    - plausible - purgecss

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,14 @@
 import { DefaultSeo } from 'next-seo'
-
 import SEO from '../next-seo.config'
+import PlausibleProvider from 'next-plausible'
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export default function MyApp({ Component, pageProps }) {
   return (
     /* Here we call NextSeo and pass our default configuration to it  */
-    <>
+    <PlausibleProvider domain={process.env.NEXT_PUBLIC_DOMAIN}>
       <DefaultSeo {...SEO} />
       <Component {...pageProps} />
-    </>
+    </PlausibleProvider>
   )
 }
